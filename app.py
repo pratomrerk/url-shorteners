db33a9f6389d2df2deff12cc01fdf813efb726d9
Pratomrerk <bpratomr@medicine.psu.ac.th>
2023-02-21 16:12:24 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,103 @@
+import os
+from flask import Flask, redirect, jsonify, request
+from flask_cors import CORS
+from redis import Redis
+import datetime as dt
+import string
+import random
+import json
+
+# pratomrerk
+# Update 21/02/2023
+
+app = Flask(__name__)
+CORS(app)
+redis_host = os.environ.get('REDIS_HOST', 'localhost')
+redis_port = os.environ.get('REDIS_PORT', 6379)
+redis_db = os.environ.get('REDIS_DB', 8)
+redis_password = os.environ.get('REDIS_PASSWORD', '')
+
+def random_string(length):
+    all_chars = string.digits + string.ascii_letters + string.ascii_uppercase
+    return ''.join(random.choice(all_chars) for m in range(length))
+
+def connect_redis():
+    return Redis(host=redis_host, port=redis_port, db=redis_db, password=redis_password) 
+
+@app.route('/<url_key>', methods=['GET'])
+def get(url_key):
+    r = connect_redis()
+    v = r.get(url_key)
+    if v is not None:
+        v = json.loads(v)
+        url = v['url']
+        v['count'] += 1
+        r.set(url_key, json.dumps(v))
+        r.close()
+        return redirect(url)
+    else:
+        return jsonify({
+            'error': 'Not found or expired'
+        }), 404
+
+
+@app.route('/new-url', methods=['GET'])
+def new_url():
+
+    url = request.args.get('url', None)
+    expire = request.args.get('expire', 60*60*24*7) # 7 days by default
+    if url is None:
+        return jsonify({
+            'error': 'URL is required'
+        }), 400
+
+    if expire is not None:
+        try:
+            expire = int(expire)
+        except ValueError:
+            return jsonify({
+                'error': 'Expire must be integer'
+            }), 400
+    
+    r = connect_redis()
+
+    while True:
+        url_key = random_string(8)
+        if not r.get(url_key):
+            break
+    
+    expired = int(dt.datetime.now().timestamp()) + expire
+    data = {
+        'short_url': f'{request.url_root}{url_key}',
+        'url': url,
+        'count': 0,
+        'expire': expired
+    }
+    #print(data)
+    r.set(url_key, json.dumps(data), ex=expire)
+
+    r.close()
+    data['check'] = f'{request.url_root}check?url-key={url_key}'
+    return jsonify(data)
+
+@app.route('/check', methods=['GET'])
+def check_count():
+    url_key = request.args.get('url-key', None)
+    if url_key is None:
+        return jsonify({
+            'error': 'url-key is required'
+        }), 400
+
+    r = connect_redis()
+    v = r.get(url_key)
+    r.close()
+    if v:
+        data = json.loads(v)
+        return jsonify(data)
+    else:
+        return jsonify({
+            'error': 'Not found or expired'
+        }), 404
+
+if __name__ == '__main__':
+    app.run(port=7788, debug=True)
